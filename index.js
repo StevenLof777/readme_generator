81574998494600a73e9ff16d133dbac1b1c8e406
Jake Finn <lof.steven@gmail.com>
2022-01-14 20:33:11 -0600

    add gitignore

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// const generateHTML = ({ name, location, github, linkedin }) =>
-//   `<!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <meta charset="UTF-8">
-//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-//   <title>Document</title>
-// </head>
-// <body>
-//   <div class="jumbotron jumbotron-fluid">
-//   <div class="container">
-//     <h1 class="display-4">Hi! My name is ${name}</h1>
-//     <p class="lead">I am from ${location}.</p>
-//     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-//     <ul class="list-group">
-//       <li class="list-group-item">My GitHub username is ${github}</li>
-//       <li class="list-group-item">LinkedIn: ${linkedin}</li>
-//     </ul>
-//   </div>
-// </div>
-// </body>
-// </html>`;
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
+
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
+
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//   return `# ${data.title}
+
+// `;
+// }
+
+// module.exports = generateMarkdown;
+
+
+const generateREADME = ({ title, description, usage, installation, contribution, test, github, email}) =>
+  `
+  # ${title}
+
+    ${description}
+
+    ## Installation
+
+    ${installation}
+
+    ## Usage
+
+    \```
+    ${usage}
+    \```
+
+
+    ## &
+
+    ## License
+    [MIT](https://choosealicense.com/licenses/mit/)
+  `;
 
 inquirer
   .prompt([
@@ -36,6 +57,11 @@ inquirer
       type: 'input',
       name: 'description',
       message: 'Give a description of what your project is about.',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide an example of how a feature would work.',
     },
     {
       type: 'input',
@@ -59,16 +85,26 @@ inquirer
     //   message: 'Enter your LinkedIn URL.',
     // },
     {
-        type: 'input',
-        name: 'github',
-        message: 'What is your github username?',
-      },
+    type: 'input',
+    name: 'github',
+    message: 'What is your github username?',
+    },
+    {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+    },
   ])
   .then((answers) => {
-      console.log(answers)
-    // const htmlPageContent = generateHTML(answers);
+    console.log(answers)
+    const READMEContent = generateREADME(answers);
 
     // fs.writeFile('README.md', htmlPageContent, (err) =>
     //   err ? console.log(err) : console.log('Successfully created index.html!')
     // );
-  });
+    // Replace READMEContent with `${process.argv[2]}\n` if it doesn't work
+
+    fs.appendFile('README.md', READMEContent, (err) =>
+    err ? console.error(err) : console.log('README.md generated.')
+);
+});
